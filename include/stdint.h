4a36d45427ca011953d0d627ebdcb5c1740c619c
Eric Biggers <ebiggers3@gmail.com>
2013-09-09 20:25:32 -0500

    Add stdint.h

---
+++ include/stdint.h
@@ -0,0 +1,16 @@
+#ifndef _STDINT_H_
+#define _STDINT_H_
+
+/* Signed and unsigned integer types of exact width  */
+
+typedef signed char          int8_t;
+typedef short                int16_t;
+typedef int                  int32_t;
+typedef long long            int64_t;
+
+typedef unsigned char        uint8_t;
+typedef unsigned short       uint16_t;
+typedef unsigned int         uint32_t;
+typedef unsigned long long   uint64_t;
+
+#endif /* _STDINT_H_ */
