55612618318d7157dcec7f3805bac7d85eb39e3e
Eric Biggers <ebiggers3@gmail.com>
2013-11-17 22:32:04 -0600

    Fix dot2ipv4()
    Correctly consider strings with fewer or more than the expected number of
    numeric fields, or any field outside the range [0, 255], to be invalid.

--- network/ipv4/dot2ipv4.c
+++ network/ipv4/dot2ipv4.c
@@ -1,26 +1,29 @@
 /**
  * @file dot2ipv4.c
- *
  */
-/* Embedded Xinu, Copyright (C) 2009.  All rights reserved. */
+/* Embedded Xinu, Copyright (C) 2009, 2013.  All rights reserved. */
 
-#include <stddef.h>
-#include <ctype.h>
 #include <ipv4.h>
 #include <network.h>
+#include <stdio.h>
 
 /**
  * @ingroup ipv4
  *
- * Convert an IP Address in dotted-decimal notation into a netaddr.
- * @param str dotted-decimal IP Address string
- * @param ip netaddr structure for conversion result
- * @return OK if conversion was successful, otherwise SYSERR
+ * Convert an IPv4 address in dotted-decimal notation into a netaddr.
+ *
+ * @param str
+ *      The dotted-decimal IPv4 address string to convert.
+ * @param ip
+ *      The netaddr structure for conversion result.
+ *
+ * @return
+ *      ::OK if conversion was successful, otherwise ::SYSERR.
  */
 syscall dot2ipv4(const char *str, struct netaddr *ip)
 {
-    char c;
-    uint count = 0, temp = 0;
+    uint o0, o1, o2, o3;
+    char tmp;
 
     /* Error check pointers */
     if ((NULL == str) || (NULL == ip))
@@ -28,26 +31,16 @@ syscall dot2ipv4(const char *str, struct netaddr *ip)
         return SYSERR;
     }
 
-    while ((count < IPv4_ADDR_LEN) && *str)
+    if (4 != sscanf(str, "%3u.%3u.%3u.%3u%c", &o0, &o1, &o2, &o3, &tmp) ||
+        o0 > 0xff || o1 > 0xff || o2 > 0xff || o3 > 0xff)
     {
-        c = *str;
-        if (isdigit(c))
-        {
-            temp = temp * 10 + (c - '0');
-        }
-        else if ('.' == c)
-        {
-            ip->addr[count] = temp;
-            temp = 0;
-            count++;
-        }
-        else
-        {
-            return SYSERR;
-        }
-        str++;
+        return SYSERR;
     }
-    ip->addr[count] = temp;
+
+    ip->addr[0] = o0;
+    ip->addr[1] = o1;
+    ip->addr[2] = o2;
+    ip->addr[3] = o3;
     ip->type = NETADDR_IPv4;
     ip->len = IPv4_ADDR_LEN;
     return OK;
